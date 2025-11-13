e8ca65d13100fec7ba1f880d5f639de792d54615
Devvrath Giri <163282868+DevvrathGiri@users.noreply.github.com>
2025-11-13 13:28:01 +0530

    middleware changes

--- middleware.js
+++ middleware.js
@@ -1,19 +1,19 @@
 const Listing = require("./models/listing");
 const Review = require("./models/review");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema, reviewSchema} = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 
 // Middleware to check if the user is logged in
 module.exports.isLoggedin = (req, res, next) => {
     if (!req.isAuthenticated()) {
-        req.session.redirectUrl = req.originalUrl; // Save the URL they are trying to access
+        req.session.redirectUrl = req.originalUrl;
         req.flash("error", "You must be logged in to create listings!");
         return res.redirect("/login");
     }
     next();
 };
 
-// Middleware to save redirect URL
+// Store redirect URL
 module.exports.saveRedirectUrl = (req, res, next) => {
     if (req.session.redirectUrl) {
         res.locals.redirectUrl = req.session.redirectUrl;
@@ -21,39 +21,48 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     next();
 };
 
-module.exports.isOwner= async(req,res,next)=>{
-    let {id} = req.params;
-    let listing = await Listing.findById(id);
-     if(!listing.owner.equals(res.locals.currUser._id)){
-         req.flash("error", "You don't have permission to edit");
-         return res.redirect(`/listings/${id}`);
-      }
-      next();
-};
-module.exports.validateListing = (req, res, next)=>{
-    let {error}= listingSchema.validate(req.body);
-    if(error){
-        let errMsg= error.details.map((el)=> el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
+// Check owner of listing
+module.exports.isOwner = async (req, res, next) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+
+    // Safe: currUser may not exist
+    if (!res.locals.currUser || !listing.owner.equals(res.locals.currUser._id)) {
+        req.flash("error", "You don't have permission to edit this listing.");
+        return res.redirect(`/listings/${id}`);
     }
+    next();
 };
-module.exports.validateReview = (req, res, next)=>{
-    let {error}= reviewSchema.validate(req.body);
-    if(error){
-        let errMsg= error.details.map((el)=> el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
+
+// Validate listing
+module.exports.validateListing = (req, res, next) => {
+    const { error } = listingSchema.validate(req.body);
+    if (error) {
+        const errMsg = error.details.map(el => el.message).join(",");
+        throw new ExpressError(400, errMsg);
     }
+    next();
 };
-module.exports.isReviewAuthor= async(req,res,next)=>{
-    let {id, reviewId} = req.params;
-    let review = await Review.findById(reviewId);
-     if(!review.author.equals(res.locals.currUser._id)){
-         req.flash("error", "You are not author of the review");
-         return res.redirect(`/listings/${id}`);
-      }
-      next();
+
+// Validate review
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const errMsg = error.details.map(el => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    next();
+};
+
+// Check author of review
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+
+    // Safe check
+    if (!res.locals.currUser || !review.author.equals(res.locals.currUser._id)) {
+        req.flash("error", "You are not authorized to delete this review.");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
 };
